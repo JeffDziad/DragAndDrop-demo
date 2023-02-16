58932227f54a356236f915f69a95e097144141a8
Jeff Dziadulewicz <jeffdziad@gmail.com>
2023-02-16 02:26:23 -0600

    Initial commit

---
+++ test-code.js
@@ -0,0 +1,50 @@
+function classElements(classId) {
+    return document.getElementsByClassName(classId);
+}
+
+function initializeDraggables() {
+    let elements = classElements("draggable");
+    for(let i = 0; i < elements.length; i++) {
+        createDraggable(elements[i]);
+    }
+}
+
+function createDraggable(elmnt) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if(document.getElementById(elmnt.id + "header")) {
+        document.getElementById(elmnt.id+"header").onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+function start() {
+    initializeDraggables();
+}
+
+window.onload = start;
