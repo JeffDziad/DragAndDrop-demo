741a7c86d97dedc7bbe5a7de35f6ca40647f8d53
Jeff Dziadulewicz <jeffdziad@gmail.com>
2023-02-23 03:09:48 -0600

    + Updates

--- DaD/DraggableClass.js
+++ DaD/DraggableClass.js
@@ -4,6 +4,10 @@ class Draggable {
         this.x2 = 0;
         this.x3 = 0;
         this.x4 = 0;
+
+        this.scrollXOffset = 0;
+        this.scrollYOffset = 0;
+
         this.element = element;
         this.element.onmousedown = this.dragMouseDown.bind(this);
     }
@@ -23,15 +27,19 @@ class Draggable {
         this.x4 = e.clientY;
         document.onmouseup = this.closeMouseDrag.bind(this);
         document.onmousemove = this.elementDrag.bind(this);
+
+        //document.onscroll = this.pageScroll.bind(this);
     }
 
     elementDrag(e) {
         e = e || window.event;
         e.preventDefault();
-        this.x1 = this.x3 - e.clientX;
-        this.x2 = this.x4 - e.clientY;
-        this.x3 = e.clientX;
-        this.x4 = e.clientY;
+        let scrollY = e.clientY + window.scrollY;
+        let scrollX = e.clientX + window.scrollX;
+        this.x1 = this.x3 - scrollX;
+        this.x2 = this.x4 - scrollY;
+        this.x3 = scrollX;
+        this.x4 = scrollY;
         this.element.style.top = (this.element.offsetTop - this.x2) + "px";
         this.element.style.left = (this.element.offsetLeft - this.x1) + "px";
     }
